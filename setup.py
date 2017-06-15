8f432ae9640a35b1918372b9c931a4183234d175
vyvojer <vyvojer@gmail.com>
2017-06-15 13:43:00 +0200

    Add setup.py and MANIFEST.ini

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='ploev',
+      version='0.1',
+      description='Python library for ProPokerTools Odds Oracle',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Utilities',
+      ],
+      url='https://github.com/vyvojer/ploev',
+      author='Alexey Londkevich',
+      author_email='vyvojer@gmail.com',
+      license='GPLv3+',
+      packages=['ploev'],
+      install_requires=['pyparsing', 'colorama'],
+      zip_safe=False)
