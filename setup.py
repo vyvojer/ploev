28f7988d8145cb63efb4fd340bffb91e9b493f48
vyvojer <vyvojer@gmail.com>
2018-07-17 21:28:01 +0200

    Change setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ploev',
-      version='0.2.0',
+      version='0.2.1',
       description='Python library for ProPokerTools Odds Oracle',
       classifiers=[
           'Development Status :: 3 - Alpha',
